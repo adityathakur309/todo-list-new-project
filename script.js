d0b22c68092c890f5bb81d63a17c5cff4b15d4a2
adityathakur309 <rajputru690@gmail.com>
2024-05-21 18:16:54 +0530

    add new project

---
+++ script.js
@@ -0,0 +1,80 @@
+let input = document.getElementById("input-field");
+let addBtn = document.querySelector(".add-btn");
+let todo_list_items = document.querySelector(".todo-item-list");
+let deleteBtn = document.querySelector(".delete")
+// add data in list 
+let localData = [];
+let addData = (e) =>{
+    createElement(input.value);
+    storeDataLocal();
+    
+
+
+}
+// end 
+// store data in local storage
+let storeDataLocal = () => {
+    localStorage.setItem("TodoItem", JSON.stringify(localData))
+} 
+// end 
+
+// create Element 
+let createElement = (value) => {
+    let div = document.createElement("div");
+   if(!localData.includes(value)) {div.classList.add("todo-item");
+    div.innerHTML = `<li>${value}</li>
+    <button class="delete">Delete</button>`;
+    localData.push(value)
+    todo_list_items.append(div);
+}
+}
+// end 
+// get data from local storage
+let getData = () => {
+    
+    let data =  JSON.parse(localStorage.getItem("TodoItem"));
+    data.forEach(
+        (curr) =>{
+            createElement(curr)
+        }
+    )
+} 
+getData();
+// end 
+// remove data 
+let removeData =(e) => {
+   let targetElement = e.target;
+   let previousElement = targetElement.previousElementSibling;
+   let parent = previousElement.parentElement;
+   if(localData.includes(previousElement.innerHTML)){
+   localData =  localData.filter(
+        (curr) => {
+            updateData(curr)
+             return curr !== previousElement.innerHTML;
+             
+        }
+    )
+    
+   }
+   
+   parent.remove();
+ 
+
+
+}
+// end
+// update data on localStorage 
+let updateData =(element) => {
+    localData.push(element)
+    localStorage.setItem("TodoItem", JSON.stringify(localData))
+}
+// end 
+todo_list_items.addEventListener("click", (e) => {
+    if(e.target.classList.contains("delete")){
+        removeData(e);
+    }
+})
+addBtn.addEventListener("click" , (e) => {
+    e.preventDefault();
+    addData(e);
+})
